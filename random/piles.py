1296aea1347a2e389166ad9293df0f0755b18dd2
Martin Gergov <martingergov1@gmail.com>
2017-11-29 22:01:04 +0100

    initial pass ignores equal values

--- random/piles.py
+++ random/piles.py
@@ -17,8 +17,14 @@ def split_pile(pile):
     A = [current,]
     del p[-1]
     split_sum -= current
+    lenpile = -len(p)
     i = -1
-    while True:
+    while True: #first pass, ignore equals
+        if i == lenpile:
+            break
+        if p[i] == A[-1]:
+            i -= 1
+            continue
         if p[i] < split_sum:
             A.append(p[i])
             split_sum -= p[i]
@@ -29,12 +35,25 @@ def split_pile(pile):
             return [A, p]
         i -= 1
 
+    i = -1
+    while True: #second pass, include all
+        if p[i] < split_sum:
+            A.append(p[i])
+            split_sum -= p[i]
+            del p[i]
+        elif p[i] == split_sum:
+            A.append(p[i])
+            del p[i]
+            return [A, p]
+        i -= 1
+
+
 print(split_pile([1]*12))
 print(split_pile([6,3,8,2,3,2,1,1]))
 print(split_pile([14,8,6]))
 print(split_pile([5,10,4,1]))
 print(split_pile([5,11,2,3,5,4,1,1]))
 print(split_pile([1,1,1,3,4]))
-
+print(split_pile([5,5,4,4,4,4]))
 
 
